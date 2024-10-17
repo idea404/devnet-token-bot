793a6e1f35949ce142f0b7ae36c974d5408ac62a
idea404 <dennis.oliver.huisman@gmail.com>
2024-10-17 15:08:04 +0200

    feat: open tg bot

--- bot.py
+++ bot.py
@@ -2,7 +2,6 @@ import logging
 import sqlite3
 from dotenv import load_dotenv
 import os
-from functools import wraps
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
@@ -25,32 +24,38 @@ load_dotenv()
 # ------------------------#
 #      Database Setup     #
 # ------------------------#
+
 # Database connection
 conn = sqlite3.connect('bot_database.db')
 cursor = conn.cursor()
 
-# Initialize database tables
+# Initialize database table
 def init_db():
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS members (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            username TEXT UNIQUE NOT NULL
-        )
-    ''')
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS urls (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             url TEXT NOT NULL,
-            email TEXT,
             handle TEXT,
             assigned_at TIMESTAMP DEFAULT NULL
         )
     ''')
-    # add Xidea404 to members if not already there
-    cursor.execute('SELECT 1 FROM members WHERE LOWER(username) = ?', ("xidea404",))
-    if cursor.fetchone() is None:
-        cursor.execute('INSERT INTO members (username) VALUES (?)', ("xidea404",))
-    conn.commit()
+
+    # == TESTS: add default data, timestamp May 1st 2024 ==
+
+    # cursor.execute('''
+    #     INSERT INTO urls (url, handle, assigned_at) VALUES
+    #         ('https://rpc.ankr.com/eth_sepolia', 'xidea404', '2024-05-01 00:00:00')
+    # ''')
+
+    # add some rpc urls with no usernames, no timestamp
+    # cursor.execute('''
+    #     INSERT INTO urls (url, handle, assigned_at) VALUES
+    #         ('https://rpc.ankr.com/eth_sepolia1', NULL, NULL),
+    #         ('https://rpc.ankr.com/eth_sepolia2', NULL, NULL),
+    #         ('https://rpc.ankr.com/eth_sepolia3', NULL, NULL)
+    # ''')
+
+    # ==== END OF TESTS ====
 
 init_db()
 
@@ -63,77 +68,26 @@ def is_member(username: str) -> bool:
     return cursor.fetchone() is not None
 
 # ------------------------#
-#      Decorators         #
-# ------------------------#
-def restricted(func):
-    """Decorator to restrict access to member users based on Telegram handle."""
-    @wraps(func)
-    async def wrapped(update: Update, context: ContextTypes.DEFAULT_TYPE, *args, **kwargs):
-        user = update.effective_user
-        if not user.username:
-            logger.warning("User ID %s does not have a username.", user.id)
-            await update.message.reply_text("You need to set a Telegram username to use this bot.")
-            return
-        if not is_member(user.username):
-            logger.warning("Unauthorized access denied for user ID %s (username: %s).", user.id, user.username)
-            await update.message.reply_text("You are not authorized to use this bot.")
-            return
-        return await func(update, context, *args, **kwargs)
-    return wrapped
-
-# ------------------------#
 #      Command Handlers   #
 # ------------------------#
-@restricted
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     """Handler for the /start command."""
-    await update.message.reply_text('Welcome! You are authorized to use this bot. Use /gettoken to receive your RPC URL. Use /addmember to add a =nil; TG user to members.')
+    await update.message.reply_text('Welcome! You are authorized to use this bot. Use /getrpcurl to receive your RPC URL. Use /getsandboxurl to receive your sandbox URL.')
 
-@restricted
-async def addmember(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    """Handler for the /addmember command."""
-    args = context.args
-
-    if len(args) != 1:
-        await update.message.reply_text('Usage: /addmember <nil_team_member_tg_handle>')
+async def get_rpc_url(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """Handler for the /getrpcurl command."""
+    # Get username from update
+    username = update.message.chat.username.lower()
+    if not username:
+        await update.message.reply_text('Please set a username to receive your RPC URL.')
         return
 
-    username = args[0]
-    username = username.replace("@", "").lower()
-
-    # Check if the user is already a member
-    cursor.execute('SELECT 1 FROM members WHERE LOWER(username) = ?', (username,))
-    if cursor.fetchone():
-        await update.message.reply_text(f'{username} is already a member.')
+    # Check if username has an RPC URL
+    cursor.execute('SELECT url FROM urls WHERE handle = ?', (username,))
+    result = cursor.fetchone()
+    if result:
+        await update.message.reply_text(f'Your RPC URL: {result[0]}')
         return
-
-    # Add the user as a member
-    try:
-        cursor.execute('INSERT INTO members (username) VALUES (?)', (username,))
-        conn.commit()
-        await update.message.reply_text(f'{username} added to members.')
-    except sqlite3.Error as e:
-        logger.error("Database error: %s", e)
-        await update.message.reply_text('Error adding user to members. Please try again.')
-
-@restricted
-async def gettoken(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    """Handler for the /gettoken command."""
-    args = context.args
-
-    if len(args) != 1:
-        await update.message.reply_text('Usage: /gettoken <developer_email_or_tg_handle>')
-        return
-
-    user_input = args[0]
-
-    # Determine if the user input is an email or a handle
-    handle = None
-    email = None
-    if user_input[0] == "@" or "@" not in user_input:
-        handle = user_input.replace("@", "").lower()
-    else:
-        email = user_input
 
     # Get the first unassigned RPC URL
     cursor.execute('''
@@ -149,18 +103,22 @@ async def gettoken(update: Update, context: ContextTypes.DEFAULT_TYPE):
         try:
             cursor.execute('''
                 UPDATE urls
-                SET handle = ?, email = ?, assigned_at = CURRENT_TIMESTAMP
+                SET handle = ?, assigned_at = CURRENT_TIMESTAMP
                 WHERE id = ?
-            ''', (handle, email, url_id))
+            ''', (username, url_id))
             conn.commit()
-            await update.message.reply_text(f'{rpc_url}')
-            logger.info("Assigned RPC URL %s to user %s", rpc_url, handle)
+            await update.message.reply_text(f'Your RPC URL: {rpc_url}')
+            logger.info("Assigned RPC URL %s to user %s", rpc_url, username)
         except sqlite3.Error as e:
             logger.error("Database error: %s", e)
             await update.message.reply_text('Error assigning RPC URL. Please try again.')
     else:
         logger.warning("No unassigned RPC URLs available")
         await update.message.reply_text('No RPC URLs available at the moment. Please try again later.')
+
+async def get_sandbox_url(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """Handler for the /getsandboxurl command."""
+    await update.message.reply_text('Start coding with our web IDE: https://explore.nil.foundation/sandbox/31edd8e9eb48ade662a04a610c0d69fb13a387c49fe283551b42f1c9ca2d3fbe')
 
 # ------------------------#
 #           Main          #
@@ -172,8 +130,8 @@ def main():
 
     # Register command handlers
     app.add_handler(CommandHandler("start", start))
-    app.add_handler(CommandHandler("addmember", addmember))
-    app.add_handler(CommandHandler("gettoken", gettoken))
+    app.add_handler(CommandHandler("getrpcurl", get_rpc_url))
+    app.add_handler(CommandHandler("getsandboxurl", get_sandbox_url))
 
     # Start the bot
     app.run_polling()
